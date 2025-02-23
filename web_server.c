06cee71bed48c79676d49faefb3c9f42a30d7316
Lachlan Murphy <lachlan.g.murphy@gmail.com>
2025-02-22 19:54:32 -0700

    web server up and running

--- web_server.c
+++ web_server.c
@@ -19,12 +19,16 @@
 #include <errno.h>
 #include <signal.h>
 #include <pthread.h>
+#include <string.h>
 #include "array.h"
 
 #define BUFFERSIZE 1024
 #define ROOT_DIR "./www"
+#define INDEX "index.html"
+// HTTP version, status, content type, content length
+#define HEADER "%s %s\r\nContent-Type: %s\r\nContent-Length: %lu\r\n\r\n"
 
-char* res = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 12\r\n\r\n";
+#define FILE_SUF_LEN 8
 
 // sigint handler
 void sigint_handler(int sig);
@@ -41,6 +45,9 @@ typedef struct {
 // multi-thread function to handle new socket connections
 void* socket_handler(void* arg);
 
+// matches a file suffix with a file type
+int find_file_type(char* file_name);
+
 /*
  * error - wrapper for perror
  */
@@ -49,7 +56,30 @@ void error(char *msg) {
 	exit(1);
 }
 
-array socks;
+// global values
+array socks; // semaphores used, thread safe
+const char* file_types[FILE_SUF_LEN] = {
+	"text/html",
+	"text/plain",
+	"image/png",
+	"image/gif",
+	"image/jpg",
+	"image/x-icon",
+	"text/css",
+	"application/javascript"
+}; // read only, thread safe
+
+// indices align with file_types
+const char* file_suff[FILE_SUF_LEN] = {
+	".html",
+	".txt",
+	".png",
+	".gif",
+	".jpg",
+	".ico",
+	".css",
+	".js"
+}; // read only, thread safe
 
 int main(int argc, char** argv) {
     int sockfd, new_socket;
@@ -150,34 +180,80 @@ void* socket_handler(void* arg) {
 	buf[strcspn(buf, "\n")] = '\0';
 
 	// parse message
-	char req[3][BUFFERSIZE]; // req[0]=method ; req[1]=URI ; req[2]=version
-	char* tmp = strtok(buf, " ");
-	memcpy(req[0], tmp, BUFFERSIZE);
-	for (int i = 1; i < 3; i++) {
-		tmp = strtok(NULL, " ");
+	char req[3][BUFFERSIZE / 2]; // req[0]=method ; req[1]=URI ; req[2]=version
+	char* tmp;
+	int parse_err = 0;
+	for (int i = 0; i < 3; i++) {
+		if (i == 0) tmp = strtok(buf, " ");
+		else tmp = strtok(NULL, " ");
+
+		if (!tmp) {
+			parse_err = 1;
+			break;
+		}
 		memcpy(req[i], tmp, BUFFERSIZE);
 	}
 
-	char file_name[BUFFERSIZE];
-	strncpy(file_name, ROOT_DIR, strlen(ROOT_DIR));
-	strcat(file_name, req[1]);
-	printf("%s\n", file_name);
+	// version may have a cairraige return on the end, replace with str terminator
+	req[2][strcspn(req[2], "\r\n")] = '\0';
 
-	// attempt to access file requested
-	if (!access(req[1], F_OK)) {
-		// file exists, send it
-		// send file
-		FILE* file = fopen(file_name, "r");
-		int n = 0;
-		send(*args->clientfd, res, strlen(res), 0);
-		while (1) {
-			n = fread(buf, 1, BUFFERSIZE, file);
-			if (n <= 0) break;
-			send(*args->clientfd, buf, n, 0);
-		}
-		fclose(file);
+	if (parse_err) {
+		// 400 Bad request
+		sprintf(buf, HEADER, "HTTP/1.1", "400 Bad Request", "text/plain", 0UL);
+		send(*args->clientfd, buf, strlen(buf), 0);
+	} else if (strncmp(req[0], "GET", strlen("GET"))) { // Check method, only get is allowed
+		// 405 Method Not Allowed
+		sprintf(buf, HEADER, req[2], "405 Method Not Allowed", "text/plain", 0UL);
+		send(*args->clientfd, buf, strlen(buf), 0);
+	} else if (strncmp("HTTP/1.0", req[2], strlen(req[2]) - 1) &&
+			   strncmp("HTTP/1.1", req[2], strlen(req[2]) - 1)) { // check HTTP version
+		// 505 HTTP Version Not Supported
+		sprintf(buf, HEADER, req[2], "505 HTTP Version Not Supported", "text/plain", 0UL);
+		send(*args->clientfd, buf, strlen(buf), 0);
 	} else {
-		error("ERROR opening file");
+		char file_name[BUFFERSIZE];
+		strncpy(file_name, ROOT_DIR, strlen(ROOT_DIR)+1);
+		strcat(file_name, req[1]);
+
+		// if we are requesting a directory, replace ending with index.html
+		if (file_name[strlen(file_name)-1] == '/') {
+			// char indx_suffix
+			strcat(file_name, (char *) &INDEX);
+		}
+
+		// attempt to access file requested
+		if (!access(file_name, F_OK) && !access(file_name, R_OK)) {
+			// file exists, send it
+			// send file
+			FILE* file = fopen(file_name, "r");
+			int n = 0;
+
+			// get file size
+			fseek(file, 0, SEEK_END);
+			unsigned long file_size = ftell(file);
+			fseek(file, 0, SEEK_SET);
+
+			sprintf(buf, HEADER, req[2], "200 OK", file_types[find_file_type(file_name)], file_size);
+			send(*args->clientfd, buf, strlen(buf), 0);
+			while (1) {
+				n = fread(buf, 1, BUFFERSIZE, file);
+				if (n <= 0) break;
+				send(*args->clientfd, buf, n, 0);
+			}
+			fclose(file);
+		} else {
+			// check whether it was a permission err or file didnt exist
+			if (access(file_name, F_OK)) {
+				// 404 file not found
+				sprintf(buf, HEADER, req[2], "404 Not Found", "text/plain", 0UL);
+				send(*args->clientfd, buf, strlen(buf), 0);
+			} else if (access(file_name, R_OK)) {
+				// 403 forbidden
+				sprintf(buf, HEADER, req[2], "403 Forbidden", "text/plain", 0UL);
+				send(*args->clientfd, buf, strlen(buf), 0);
+			}
+		}
+
 	}
 
 	close(*args->clientfd);
@@ -189,4 +265,17 @@ void* socket_handler(void* arg) {
 	free(to_free);
 	free(args);
 	return NULL;
+}
+
+int find_file_type(char* file_name) {
+	// get suffic of file name
+	char* suf = strrchr(file_name, '.');
+	if (!suf || suf == file_name) return 1; // by default do plain text
+
+	// match with suffixes from file_suf array
+	for (int i = 0; i < FILE_SUF_LEN; i++) {
+		if (strncmp(suf, file_suff[i], strlen(suf)) == 0) return i;
+	}
+
+	return 1; // by default just do plain text
 }
